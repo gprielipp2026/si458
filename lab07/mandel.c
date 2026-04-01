4282c8763bfebfb2fabadde3188d7075a4ca8a4e
gprielipp2026 <m265112@usna.edu>
2026-04-01 10:40:15 -0400

    done

--- lab07/mandel.c
+++ lab07/mandel.c
@@ -8,7 +8,7 @@ uint8_t inMandel(double x, double y)
 {
     double complex c = x + y*I;
     double complex Zn = 0;
-
+    
     for(uint8_t i = 0; i < 100; i++)
     {
         Zn = Zn * Zn + c;
@@ -23,12 +23,12 @@ uint8_t inMandel(double x, double y)
     return mag < 4.0;
 }
 
-void mandelSet(uint8_t* array, uint32_t w, uint32_t h, double xmax, double xmin, double ymax, double ymin, uint32_t processors)
+void mandelSet(uint8_t* array, const uint32_t w, const uint32_t h, double xmax, double xmin, double ymax, double ymin, uint32_t processors)
 {
-    double dx = (xmax - xmin) / w;
-    double dy = (ymax - ymin) / h;
+    double dx = (xmax - xmin) / (double)w;
+    double dy = (ymax - ymin) / (double)h;
 
-    # pragma omp parallel num_threads(processors)
+    # pragma omp parallel num_threads(processors) shared(w) shared(h) shared(dx) shared(dy) shared(xmin) shared(ymin)
     {
         uint32_t size = omp_get_num_threads();
         uint32_t rank = omp_get_thread_num();
@@ -37,18 +37,21 @@ void mandelSet(uint8_t* array, uint32_t w, uint32_t h, double xmax, double xmin,
 
         uint32_t ystart = chunksize * rank;
         uint32_t yend = chunksize * (rank + 1);
-
+        
         // give extra work to last thread
-        if(rank == size - 1) 
+        if(rank == (size - 1))
         {
             yend = h;
         }
+       
+        // debug 
+        printf("%2d/%2d: %dx%d   %4d - %4d    (%4d)\n", rank, size, w, h, ystart, yend, yend - ystart);
 
         for(uint32_t y = ystart; y < yend; y++) 
         {
             for(uint32_t x = 0; x < w; x++) 
             {
-                array[x + y*h] = inMandel(xmin + x*dx, ymin + y*dy);
+                *(array + x + y*h) = inMandel(xmin + x*dx, ymin + y*dy);
             }
         }
     }
@@ -68,14 +71,19 @@ void print(uint8_t *array, uint32_t w, uint32_t h)
 
 int main(int argc, char* argv[])
 {
+  if(argc != 4) {
+    printf("usage: %s <width> <height> <processors>\n", argv[0]);
+    exit(0);
+  }
+
     // should hhave a test here
-    uint32_t w=1000, h=850, p=1;
+    uint32_t w=atoi(argv[1]), h=atoi(argv[2]), p=atoi(argv[3]);
     double xmax=1.0, xmin=-2.0, ymax=1.0, ymin=-1.0;
     uint8_t* array = calloc(w*h, sizeof(uint8_t));
     
     mandelSet(array, w, h, xmax, xmin, ymax, ymin, p);
 
-    print(array, w, h);
+    /*print(array, w, h);*/
 
     free(array);
 
--- lab07/mandelWrapper.py
+++ lab07/mandelWrapper.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+import sys
 from ctypes import *
 import numpy as np
 import matplotlib.pyplot as plt
@@ -23,7 +23,7 @@ def mandelSet(w, h, xmax=1.0, xmin=-2.0, ymax=1.0, ymin=-1.0, p=1):
 
     func(ptr, w, h, xmax, xmin, ymax, ymin, p)
  
-    array = array.reshape((h.value, w.value))
+    array = array.reshape((w.value, h.value))
 
     return array
     
@@ -41,15 +41,23 @@ def cprintMandel(arr, w, h):
     func(ptr, w, h)
 
 if __name__ == "__main__":
+    if len(sys.argv) != 4:
+        print(f'usage: {sys.argv[0]} <width> <height> <processors>')
+        sys.exit(0)
+
     # grab array from mandelSet
-    w = 1000
-    h = 850
-    array = mandelSet(w,h, p=2)
+    w = int(sys.argv[1]) 
+    h = int(sys.argv[2])
+    p = int(sys.argv[3])
+
+    array = mandelSet(w,h, p=p)
 
     #cprintMandel(array, w, h)
 
     # plot code goes below
-    plt.imshow(array) 
-    plt.title('Mandelbrot Set')
+    #filename = f'imgs/{w}x{h}.png' 
+    #plt.imsave(filename, array, cmap='gray', dpi=300)
+
+    plt.imshow(array, cmap='gray')
     plt.show()
 
